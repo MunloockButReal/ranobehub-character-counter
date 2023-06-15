aec241abbb043809a8e77bad49147cb9cc3dc19b
MunloockButReal <munloock@gmail.com>
2023-06-15 17:02:50 +0300

    Completed function for replacing character counting and some other small changes

--- chrome/content.js
+++ chrome/content.js
@@ -56,9 +56,53 @@ function initializeContentScript() {
 	}
 }
 
-// Check if the page URL matches the specified domain
-if (window.location.href.includes('ranobehub.org/ranobe/')) {
+function replaceTotalSymbols() {
+	// PC segment
+	let pcElement = $('.book-meta-value.book-stats');
+
+	let pcChapterElement = pcElement.find('strong');
+	let pcAuthorSheetsElement = pcElement.find('i.question.outline.circle.icon.__tooltip');
+	let pcOriginalBookElement = pcElement.find('.book-stats__original');
+
+	// Mobile segment
+	let mobileElement = $('#section-information');
+	let mobileBookNotes = mobileElement.find('.book-notes').find('p');
+	let mobileLinks = mobileElement.find('.ui.small.feed.ranobe-links');
+
+	mobileElement.find('.book-notes').empty();
+	let mobileOriginal = mobileElement.find('p')[1];
+	let mLinks = $(`<strong>Ссылки:</strong>`).append(mobileLinks);
+	mobileElement.empty();
+
+	// Extract the chapter and author's sheets information
+	let chapters = pcChapterElement.text().trim();
+	let authorSheetsData = pcAuthorSheetsElement.attr('data-tippy-content');
+	let authorSheets = authorSheetsData.match(/\d+/g)[1] || 0; // Extracts all numbers from the string
+
+	let totalSymbols = authorSheets * 40000;
+	let totalSymbolsM = (totalSymbols / 1000000).toFixed(2);
+	let averageSymbols = Math.floor(totalSymbols / chapters);
+
+	pcElement.html(`<p><strong>${chapters}</strong> глав на русском</p>
+	<p style="font-size: 12px; color: whitesmoke"><strong>${totalSymbolsM}M</strong> знаков, <strong>${authorSheets}</strong> авторских листов, (${averageSymbols} в среднем)</p>`);
+
+	let mobileTranslated = `<p><strong>Переведено:</strong> ${chapters} глав; ${totalSymbolsM}M зн. ${authorSheets} а.л., (${averageSymbols} в среднем)</p>`;
+
+	pcElement.append(pcOriginalBookElement);
+	mobileElement.append(mobileBookNotes, mobileTranslated, mobileOriginal, mLinks);
+}
+// Checking if the current page is a chapter page
+let urlPatternChapter = /ranobehub.org\/ranobe\/\d+\/\d+\/\d+/;
+
+if (urlPatternChapter.test(window.location.href)) {
 	initializeContentScript();
+}
+
+// Checking if the current page is a book page
+let urlPatternBookPage = /\/ranobehub.org\/ranobe\/\d+-\w+/;
+
+if (urlPatternBookPage.test(window.location.href)) {
+	replaceTotalSymbols();
 }
 
 //
@@ -66,107 +110,16 @@ if (window.location.href.includes('ranobehub.org/ranobe/')) {
 //
 //
 //
-
-// function apiText(chapterID) {
-// 	let returnData = '';
-// 	fetch(`https://ranobehub.org/api/chapter/${chapterID}`)
-// 	.then((responce) => responce.json())
-// 	.then((data) => {
-// 		returnData = data.chapter.text.replace(new RegExp(`<.*?>`, 'g'), '');
-// 	});
-// 	return returnData;
-// }
-
-// console.log(apiText(30044));
-
 /* 
-$().ready(function () {
+function apiText(chapterID) {
+	let returnData = '';
+	fetch(`https://ranobehub.org/api/chapter/${chapterID}`)
+	.then((responce) => responce.json())
+	.then((data) => {
+		returnData = data.chapter.text.replace(new RegExp(`<.*?>`, 'g'), '');
+	});
+	return returnData;
+}
 
-
-	//
-	//
-	//
-	//
-	//
-
-	// Function to count characters in a text
-	function countCharacters(text) {
-		const charactersWithSpaces = text.length;
-		const charactersWithoutSpaces = text.replace(/\s/g, '').length;
-		const words = charactersWithSpaces !== 0 ? text.trim().split(/\s+/).length : 0;
-		return {
-			charactersWithSpaces,
-			charactersWithoutSpaces,
-			words,
-		};
-	}
-
-	// Function to count images in a chapter
-	function countImages(chapterElement) {
-		const imageElements = chapterElement.find('img');
-		return imageElements.length;
-	}
-
-	// Function to update chapter title with character count, image count, and word count
-	function updateChapterTitle(chapterElement) {
-		const chapterTitleElement = chapterElement.find('h1.ui.header');
-		if (chapterTitleElement.length) {
-			const chapterText = chapterTitleElement.text();
-			const chapterContainer = chapterElement.data('container');
-			console.log(chapterContainer);
-			const apiUrl = `https://ranobehub.org/api/chapter/${chapterContainer}`;
-
-			// Fetch the chapter content from the API
-			$.get(apiUrl)
-				.done((chapterContent) => {
-					const { charactersWithSpaces, charactersWithoutSpaces, words } = countCharacters(
-						chapterContent.chapter.text.replace(new RegExp(`<.*?>`, 'g'), '').trim()
-					);
-					const imagesCount = countImages(chapterElement);
-					const countText = `Символов: ${charactersWithSpaces}, Символов без пробелов: ${charactersWithoutSpaces}, Слов: ${words}, Картинок: ${imagesCount}`;
-					chapterTitleElement.html(
-						`${chapterText}<br><span style="font-size: 12px; color: #d4d4d4">${countText}</span>`
-					);
-					console.log(chapterContent.chapter.name, countText, chapterContent.chapter.text.slice(0, 100));
-					console.log(chapterContent.chapter.text.replace(new RegExp(`<.*?>`, 'g'), '').trim().length);
-				})
-				.fail((error) => {
-					console.error('Failed to fetch chapter content:', error);
-				});
-		}
-	}
-
-	// Function to handle the mutation observer
-	function handleMutations(mutationsList, observer) {
-		mutationsList.forEach((mutation) => {
-			const addedNodes = Array.from(mutation.addedNodes);
-			addedNodes.forEach((addedNode) => {
-				if (addedNode.nodeType === Node.ELEMENT_NODE) {
-					const chapterElements = $(addedNode).find('.ui.text.container');
-					chapterElements.each((_, chapterElement) => {
-						updateChapterTitle($(chapterElement));
-					});
-				}
-			});
-		});
-	}
-
-	// Initialize the mutation observer for chapter elements
-	function initializeContentScript() {
-		const chapterElements = $('.ui.text.container');
-		chapterElements.each((_, chapterElement) => {
-			updateChapterTitle($(chapterElement));
-		});
-
-		const appContainer = $('#app');
-		if (appContainer.length) {
-			const observer = new MutationObserver(handleMutations);
-			observer.observe(appContainer[0], { childList: true, subtree: true });
-		}
-	}
-
-	// Check if the page URL matches the specified domain
-	if (window.location.href.includes('ranobehub.org/ranobe/')) {
-		initializeContentScript();
-	}
-}); */
+console.log(apiText(30044));
+ */
