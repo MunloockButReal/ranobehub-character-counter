cf98a8a5c869a114dd74f22837ff8dcd4b79b6f6
MunloockButReal <munloock@gmail.com>
2023-07-16 18:56:43 +0300

    WIP bookmark functionality & popup

--- chrome/content.js
+++ chrome/content.js
@@ -1,75 +1,179 @@
-myStorage = window.localStorage;
-// Doing this because stupid site overwriting my css. Why, chrome?
-const userAgent = window.navigator.userAgent.toLowerCase();
+$(document).ready(function () {
+	const userAgent = window.navigator.userAgent.toLowerCase();
+	if (userAgent.indexOf('chrome') > -1 && !!window.chrome) {
+		const link = $('<link>')
+			.attr('rel', 'stylesheet')
+			.attr('type', 'text/css')
+			.attr('href', chrome.runtime.getURL('styles.css'));
+		$('head').append(link);
+	}
 
-if (userAgent.indexOf('chrome') > -1 && !!window.chrome) {
-	const link = document.createElement('link');
-	link.setAttribute('rel', 'stylesheet');
-	link.setAttribute('type', 'text/css');
-	link.setAttribute('href', chrome.runtime.getURL('styles.css'));
-	document.querySelector('head').appendChild(link);
-}
-console.log(userAgent);
-// User-customizable settings
-let readingSpeed = 180;
-let bookmarkBackgroundColor;
-let bookmarkTextColor;
+	let readingSpeed = 180; // User-customizable setting
+	let bookmarkBackgroundColor;
+	let bookmarkTextColor;
 
-//
-//
-//
+	// Checking if the current page is a chapter page
+	let urlPatternChapter = /ranobehub.org\/ranobe\/\d+\/\d+\/\d+/;
+	// Checking if the current page is a book page
+	let urlPatternBookPage = /\/ranobehub.org\/ranobe\/\d+-\w+/;
+	// Main logic based on URL patterns
 
-// Function to count characters in a text
-function countCharacters(text) {
-	const charactersWithSpaces = text.length;
-	const charactersWithoutSpaces = text.replace(/\s/g, '').length;
-	const words = charactersWithSpaces !== 0 ? text.trim().split(/\s+/).length : 0;
-	return {
-		charactersWithSpaces,
-		charactersWithoutSpaces,
-		words,
-	};
-}
+	if (urlPatternChapter.test(window.location.href)) {
+		initializeContentScript();
+	}
 
-// Function to count images in a chapter
-function countImages(chapterElement) {
-	const imageElements = chapterElement.querySelectorAll('img');
-	return imageElements.length;
-}
+	if (urlPatternBookPage.test(window.location.href)) {
+		replaceTotalSymbols();
+	}
+	// Function to count characters in a text
+	function countCharacters(text) {
+		const charactersWithSpaces = text.length;
+		const charactersWithoutSpaces = text.replace(/\s/g, '').length;
+		const words = charactersWithSpaces !== 0 ? text.trim().split(/\s+/).length : 0;
+		return {
+			charactersWithSpaces,
+			charactersWithoutSpaces,
+			words,
+		};
+	}
 
-// Function to update chapter title with character count, image count, and word count
-function updateChapterTitle(chapterElement) {
-	const chapterTitleElement = chapterElement.querySelector('h1.ui.header');
-	let chapterID;
-	if (chapterTitleElement) chapterID = chapterTitleElement.getAttribute('data-id');
+	// Function to count images in a chapter
+	function countImages(chapterElement) {
+		const imageElements = chapterElement.find('img');
+		return imageElements.length;
+	}
 
-	if (chapterTitleElement) {
-		const chapterText = chapterTitleElement.innerText;
-		const chapterAllP = chapterElement.querySelectorAll('p');
-		const chapterContent = Array.from(chapterAllP)
-			.map((p) => p.textContent)
-			.join(' ');
+	// Function to update chapter title with character count, image count, and word count
+	function updateChapterTitle(chapterElement) {
+		const chapterTitleElement = chapterElement.find('h1.ui.header');
 
-		// Adding P bookmark
+		let chapterID;
+		if (chapterTitleElement) chapterID = chapterTitleElement.attr('data-id');
+
+		if (chapterTitleElement) {
+			const chapterText = chapterTitleElement.text();
+			const bookName = $('.b-breadcrumbs li a')[1].text.replaceAll('\n', '');
+			const chapterAllP = chapterElement.find('p');
+			const chapterContent = Array.from(chapterAllP)
+				.map((p) => $(p).text())
+				.join(' ');
+
+			// Adding P bookmark
+			if (chapterID) paragraphBookmarkFunction(chapterAllP, chapterID, chapterText, bookName);
+
+			const { charactersWithSpaces, charactersWithoutSpaces, words } = countCharacters(chapterContent.trim());
+
+			const readingSpeedValue = Math.round(words / readingSpeed).toString();
+			const readingSpeedFormat =
+				readingSpeedValue +
+				(readingSpeedValue[readingSpeedValue.length - 1] == 1
+					? ' минуту'
+					: readingSpeedValue[readingSpeedValue.length - 1] == 2
+					? ' минуты'
+					: ' минут');
+			const imagesCount = countImages(chapterElement);
+			const countText = `Символов: ${charactersWithSpaces}, Символов без пробелов: ${charactersWithoutSpaces}, Слов: ${words}, Картинок: ${imagesCount}. 
+		<br> Примерное время чтения: ${readingSpeedFormat} </br>`;
+			chapterTitleElement.html(`${chapterText}<br><span style="font-size: 12px; color: #d4d4d4">${countText}</span>`);
+		}
+	}
+
+	// Function to handle the mutation observer
+	function handleMutations(mutationsList, observer) {
+		mutationsList.forEach((mutation) => {
+			const addedNodes = Array.from(mutation.addedNodes);
+			addedNodes.forEach((addedNode) => {
+				if (addedNode.nodeType === Node.ELEMENT_NODE) {
+					const chapterElements = $(addedNode).find('.ui.text.container');
+					chapterElements.each((index, chapterElement) => {
+						updateChapterTitle($(chapterElement));
+					});
+				}
+			});
+		});
+	}
+
+	// Initialize the mutation observer for chapter elements
+	function initializeContentScript() {
+		const chapterElements = $('.ui.text.container');
+		chapterElements.each((index, chapterElement) => {
+			updateChapterTitle($(chapterElement));
+		});
+
+		const appContainer = $('#app');
+		if (appContainer.length > 0) {
+			const observer = new MutationObserver(handleMutations);
+			observer.observe(appContainer[0], { childList: true, subtree: true });
+		}
+	}
+
+	function replaceTotalSymbols() {
+		let pcElement = $('.book-meta-value.book-stats');
+		let pcChapterElement = pcElement.find('strong');
+		let pcAuthorSheetsElement = pcElement.find('i.question.outline.circle.icon.__tooltip');
+		let pcOriginalBookElement = pcElement.find('.book-stats__original');
+
+		let mobileElement = $('#section-information');
+		let mobileBookNotes = mobileElement.find('.book-notes').find('p');
+		let mobileLinks = mobileElement.find('.ui.small.feed.ranobe-links');
+
+		let chapters = pcChapterElement.text().trim();
+		let authorSheetsData = pcAuthorSheetsElement.attr('data-tippy-content');
+		let authorSheets = (authorSheetsData.match(/\d+/g) || [0])[1]; // Extracts the second number from the string or default to 0
+
+		let totalSymbols = authorSheets * 40000;
+		let totalSymbolsM = (totalSymbols / 1000000).toFixed(2);
+		let averageSymbols = Math.floor(totalSymbols / chapters);
+
+		pcElement
+			.html(
+				`<p><strong>${chapters}</strong> глав на русском</p>
+		<p style="font-size: 12px; color: whitesmoke"><strong>${totalSymbolsM}M</strong> знаков, <strong>${authorSheets}</strong> авторских листов, (${averageSymbols} в среднем)</p>`
+			)
+			.append(pcOriginalBookElement);
+
+		let mobileTranslated = $(
+			`<p><strong>Переведено:</strong> ${chapters} глав; ${totalSymbolsM}M зн. ${authorSheets} а.л., (${averageSymbols} в среднем)</p>`
+		);
+
+		let mobileOriginal = mobileElement.find('p:contains("В оригинале")');
+		let mLinks = $('<strong>Ссылки:</strong>').append(mobileLinks);
+
+		mobileElement.empty().append(mobileBookNotes, mobileTranslated, mobileOriginal, mLinks);
+	}
+
+	function paragraphBookmarkFunction(chapterAllP, chapterID, chapterHeader, bookName) {
 		let prevElement = null;
-		let pBookmark = JSON.parse(myStorage.getItem('pBookmark')) || {};
 		let url = window.location.href.split('/').slice(-3);
+		let bookID = url[0];
+		let pBookmark = JSON.parse(localStorage.getItem('pBookmark')) || {};
 
 		console.log(pBookmark);
-		if (typeof pBookmark[chapterID] == 'number') {
-			p = chapterAllP[pBookmark[chapterID]];
+
+		pBookmark = {
+			[`${bookID} - ${bookName}`]: {
+				[chapterHeader]: {
+					'Full Path': url.join('/'),
+					[chapterID]: {
+						index: 0,
+					},
+				},
+			},
+		};
+		pBookmark[`${bookID} - ${bookName}`][chapterHeader]['text'] =
+			chapterAllP[pBookmark[`${bookID} - ${bookName}`][chapterHeader][chapterID].index].innerText;
+
+		console.log(pBookmark);
+		if (typeof pBookmark[bookID][chapterHeader][chapterID] == 'number') {
+			p = chapterAllP[pBookmark[bookID][chapterHeader][chapterID]];
 			$(p).addClass('paragraph-bookmark');
 			prevElement = p;
 		}
 
-		chapterAllP.forEach((p) => {
-			// $(p).append($(document.createElement('div')).addClass('paragraph-bookmark2'));
-
-			// $(p).on('click', () => {
+		chapterAllP.each((index, p) => {
 			$(p).on('click', () => {
-				// $(p).on('click', '.paragraph-bookmark2', () => {
 				console.log(p);
-				pBookmark[chapterID] = Array.from(chapterAllP).indexOf(p);
+				pBookmark[bookID][chapterHeader][chapterID] = Array.from(chapterAllP).indexOf(p);
 
 				if (prevElement !== null) {
 					$(prevElement).removeClass('paragraph-bookmark');
@@ -79,105 +183,14 @@ function updateChapterTitle(chapterElement) {
 				if (prevElement !== null && prevElement == p) {
 					$(prevElement).removeClass('paragraph-bookmark');
 					prevElement = null;
-					delete pBookmark[chapterID];
+					delete pBookmark[bookID][chapterHeader][chapterID];
 				} else prevElement = p;
 
-				myStorage.setItem('pBookmark', JSON.stringify(pBookmark));
+				localStorage.setItem('pBookmark', JSON.stringify(pBookmark));
 			});
 		});
-
-		// End of P Bookmark
-
-		const { charactersWithSpaces, charactersWithoutSpaces, words } = countCharacters(chapterContent.trim());
-
-		const readingSpeedValue = Math.round(words / readingSpeed).toString();
-		const readingSpeedFormat =
-			readingSpeedValue +
-			(readingSpeedValue[readingSpeedValue.length - 1] == 1
-				? ' минуту'
-				: readingSpeedValue[readingSpeedValue.length - 1] == 2
-				? ' минуты'
-				: ' минут');
-		const imagesCount = countImages(chapterElement);
-		const countText = `Символов: ${charactersWithSpaces}, Символов без пробелов: ${charactersWithoutSpaces}, Слов: ${words}, Картинок: ${imagesCount}. 
-		<br> Примерное время чтения: ${readingSpeedFormat} </br>`;
-		chapterTitleElement.innerHTML = `${chapterText}<br><span style="font-size: 12px; color: #d4d4d4">${countText}</span>`;
 	}
-}
-
-// Function to handle the mutation observer
-function handleMutations(mutationsList, observer) {
-	mutationsList.forEach((mutation) => {
-		const addedNodes = Array.from(mutation.addedNodes);
-		addedNodes.forEach((addedNode) => {
-			if (addedNode.nodeType === Node.ELEMENT_NODE) {
-				const chapterElements = Array.from(addedNode.querySelectorAll('.ui.text.container'));
-				chapterElements.forEach((chapterElement) => updateChapterTitle(chapterElement));
-			}
-		});
-	});
-}
-
-// Initialize the mutation observer for chapter elements
-function initializeContentScript() {
-	const chapterElements = Array.from(document.querySelectorAll('.ui.text.container'));
-	chapterElements.forEach((chapterElement) => updateChapterTitle(chapterElement));
-
-	const appContainer = document.getElementById('app');
-	if (appContainer) {
-		const observer = new MutationObserver(handleMutations);
-		observer.observe(appContainer, { childList: true, subtree: true });
-	}
-}
-
-function replaceTotalSymbols() {
-	let pcElement = $('.book-meta-value.book-stats');
-	let pcChapterElement = pcElement.find('strong');
-	let pcAuthorSheetsElement = pcElement.find('i.question.outline.circle.icon.__tooltip');
-	let pcOriginalBookElement = pcElement.find('.book-stats__original');
-
-	let mobileElement = $('#section-information');
-	let mobileBookNotes = mobileElement.find('.book-notes').find('p');
-	let mobileLinks = mobileElement.find('.ui.small.feed.ranobe-links');
-
-	let chapters = pcChapterElement.text().trim();
-	let authorSheetsData = pcAuthorSheetsElement.attr('data-tippy-content');
-	let authorSheets = (authorSheetsData.match(/\d+/g) || [0])[1]; // Extracts the second number from the string or default to 0
-
-	let totalSymbols = authorSheets * 40000;
-	let totalSymbolsM = (totalSymbols / 1000000).toFixed(2);
-	let averageSymbols = Math.floor(totalSymbols / chapters);
-
-	pcElement
-		.html(
-			`<p><strong>${chapters}</strong> глав на русском</p>
-	  <p style="font-size: 12px; color: whitesmoke"><strong>${totalSymbolsM}M</strong> знаков, <strong>${authorSheets}</strong> авторских листов, (${averageSymbols} в среднем)</p>`
-		)
-		.append(pcOriginalBookElement);
-
-	let mobileTranslated = $(
-		`<p><strong>Переведено:</strong> ${chapters} глав; ${totalSymbolsM}M зн. ${authorSheets} а.л., (${averageSymbols} в среднем)</p>`
-	);
-
-	let mobileOriginal = mobileElement.find('p:contains("В оригинале")');
-	let mLinks = $('<strong>Ссылки:</strong>').append(mobileLinks);
-
-	mobileElement.empty().append(mobileBookNotes, mobileTranslated, mobileOriginal, mLinks);
-}
-
-// Checking if the current page is a chapter page
-let urlPatternChapter = /ranobehub.org\/ranobe\/\d+\/\d+\/\d+/;
-
-if (urlPatternChapter.test(window.location.href)) {
-	initializeContentScript();
-}
-
-// Checking if the current page is a book page
-let urlPatternBookPage = /\/ranobehub.org\/ranobe\/\d+-\w+/;
-
-if (urlPatternBookPage.test(window.location.href)) {
-	replaceTotalSymbols();
-}
+});
 
 //
 //
